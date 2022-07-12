33a07d2e8d3e75fa2969885cb53db9171486a1b8
dinamashmush <dina.Mashmush@carmel6000.amitnet.org>
2022-07-12 17:37:11 +0300

    .

--- exercise1/server/app.js
+++ exercise1/server/app.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+app.get('/sign-up', (req, res) => {
+  /* 
+      write your code here
+  */
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
